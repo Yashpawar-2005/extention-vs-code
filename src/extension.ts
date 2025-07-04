c734864cfaf7f1d349aba58f3e1b1e6ee418e5b4
Yashpawar-2005 <yashpawar12312@gmail.com>
2025-07-04 15:54:06 +0530

    added the command and file now checking

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,44 @@
 import * as vscode from 'vscode';
 import express from 'express'; // works with "type": "module" OR tsconfig settings
-
+import path from 'path';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Extension "start" is now active!');
 
-	// Register a command
+	
 	const disposable = vscode.commands.registerCommand('start.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from start!');
 	});
 	context.subscriptions.push(disposable);
 
-	// ✅ Start Express inside activation
+	
 	const app = express();
 	app.use(express.json());
 
 	app.get('/', (req, res) => {
 		res.json({ message: "hi" });
 	});
+
+	app.post('/command',async(req,res)=>{
+		const command:string=req.body.command;
+		const aiTerminal = vscode.window.createTerminal({
+		name: "AI Terminal",
+		hideFromUser: false
+	});
+		aiTerminal.show();
+		await vscode.commands.executeCommand("AI Terminal",command)
+	})
+	app.post('/file',async(req,res)=>{
+		const path=req.body.path
+		const content=req.body.content
+		const Uri=vscode.Uri.file(path)
+		const dirname=path.dirname(path)
+		  try {
+		await vscode.workspace.fs.stat(vscode.Uri.file(dirname));
+	  } catch {
+		await vscode.workspace.fs.createDirectory(vscode.Uri.file(dirname));
+	  }
+		await vscode.workspace.fs.writeFile(path,Buffer.from(content, 'utf8'))
+	})
 
 	app.listen(9090, () => {
 		console.log('Express server running on http://localhost:5173');
